0b53d0798097769a6987a3f4619e805b42c26b98
bksubhuti <bksubhuti@gmail.com>
2025-07-07 12:27:33 +0530

    new version to store 92

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -2,7 +2,7 @@ const String highlightTagName = 'highlight';
 
 class DatabaseInfo {
   DatabaseInfo._();
-  static const int version = 50;
+  static const int version = 51;
   static const String fileName = 'tipitaka_pali.db';
 }
 
